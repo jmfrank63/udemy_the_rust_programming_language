19426c54e2364afc128d084553da21fb201c22c4
Johannes Maria Frank <jmfrank63@gmail.com>
2021-06-21 22:58:19 +0100

    Added section4 match

---
+++ section4/matches/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x = "Apple";
+    match x {
+        "Apple" => println!("Great brand!"),
+        "Linux" => println!("Even better"),
+        _ => println!("Some other OS system"),
+    }
+}
